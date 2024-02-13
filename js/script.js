3b843b476b6828e6baa6143c8e5e06202373a046
Lins <110426000+Lins-T@users.noreply.github.com>
2024-02-13 00:51:51 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
+'use strict';
 
+const Quest = document.querySelectorAll('.quest'),
+      questions = document.querySelectorAll('.questions');
+const answer = document.querySelectorAll('.Answer');
+const showBtns = document.querySelectorAll('.show'),
+      closeBtns = document.querySelectorAll('.close');
+
+let showArr = Array.from(showBtns),
+    closeArr = Array.from(closeBtns);
+let ans = Array.from(answer),
+    question = Array.from(Quest);
+
+question.forEach(quest => {
+  quest.addEventListener('click', () => {
+    questions[question.indexOf(quest)].classList.toggle('showing');
+
+    if (questions[question.indexOf(quest)].classList.contains('btnClose') || questions[question.indexOf(quest)].classList.contains('btnShow')) {
+      questions[question.indexOf(quest)].classList.remove('btnClose');
+      questions[question.indexOf(quest)].classList.remove('btnShow');
+      questions[question.indexOf(quest)].classList.toggle('showing');
+    }
+  }, false)
+})
+
+showArr.forEach(btnz => {
+  btnz.addEventListener('click', () => {
+    questions[showArr.indexOf(btnz)].classList.toggle('btnShow');
+
+    if (questions[showArr.indexOf(btnz)].classList.contains('showing') || questions[showArr.indexOf(btnz)].classList.contains('btnClose')) {
+      questions[showArr.indexOf(btnz)].classList.remove('btnClose');
+      questions[showArr.indexOf(btnz)].classList.remove('showing');
+      questions[showArr.indexOf(btnz)].classList.toggle('btnShow');
+    }
+  }, false)
+})
+
+closeArr.forEach(btnz => {
+  btnz.addEventListener('click', () => {
+    questions[closeArr.indexOf(btnz)].classList.toggle('btnClose');
+
+    if (questions[closeArr.indexOf(btnz)].classList.contains('showing') || questions[closeArr.indexOf(btnz)].classList.contains('btnShow')) {
+      questions[closeArr.indexOf(btnz)].classList.remove('btnShow');
+      questions[closeArr.indexOf(btnz)].classList.remove('showing');
+      questions[closeArr.indexOf(btnz)].classList.toggle('btnClose')
+    }
+  }, false)
+})
+
+/* I wanted 50 lines and I got it */
